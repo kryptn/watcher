1fc81e64b70d1d211fce1bf669215e1bd7931b72
kryptn <kryptn@gmail.com>
2024-02-08 01:41:22 -0700

    initial control cli added

---
+++ cli/src/main.rs
@@ -0,0 +1,16 @@
+use serde_dynamo::{to_item, Item};
+
+use watcher::types::Endpoint;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Hello, world!");
+
+    let endpoint = Endpoint::mock();
+    let serialized = serde_json::to_string(&endpoint).unwrap();
+    println!("fake endpoint: \n\t{}", serialized);
+
+    let dynamodb_serialized: Item = to_item(endpoint.clone())?;
+    println!("dynamodb endpoint: \n\t{:#?}", dynamodb_serialized);
+
+    Ok(())
+}
