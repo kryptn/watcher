142dcba79fd832ab5241eebd7acdfb63748a28f7
kryptn <kryptn@gmail.com>
2024-02-11 01:26:49 -0700

    add sns, sqs, and s3

--- watcher/src/types/events.rs
+++ watcher/src/types/events.rs
@@ -1,13 +1,17 @@
 use serde::{Deserialize, Serialize};
 
+use super::Observation;
+
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ScheduledObservation {
     pub endpoint_id: String,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct EndpointBroadcast {
+pub struct EndpointObserved {
     pub endpoint_id: String,
     pub observation_id: String,
-    pub sink_id: String,
+    pub observation: Observation,
 }
+
