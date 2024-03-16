20ca2c7f06b772ccedfde7b1407bc9645c942576
kryptn <kryptn@gmail.com>
2024-03-16 01:13:42 -0600

    fmt

--- watcher/src/types/events.rs
+++ watcher/src/types/events.rs
@@ -1,12 +1,19 @@
 use serde::{Deserialize, Serialize};
 
-
 #[derive(Clone, Debug, Serialize, Deserialize)]
 #[non_exhaustive]
 #[serde(tag = "command", content = "data")]
 pub enum Event {
-    SourceObserved { source_id: String, state_id: String },
-    ChangeMeasured { source_id: String, signal_id: String },
-    SignalSent { signal_id: String, sink_id: String },
-
+    SourceObserved {
+        source_id: String,
+        state_id: String,
+    },
+    ChangeMeasured {
+        source_id: String,
+        signal_id: String,
+    },
+    SignalSent {
+        signal_id: String,
+        sink_id: String,
+    },
 }
